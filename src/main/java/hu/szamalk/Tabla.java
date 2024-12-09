37fa967d692294679143dad07d0340d5c2dde36d
Csorba Eszter Katinka <csorba.e.katalin@dk.akkszalezi.hu>
2024-12-09 13:40:10 +0100

    f1 3

--- src/main/java/hu/szamalk/Tabla.java
+++ src/main/java/hu/szamalk/Tabla.java
@@ -1,4 +1,20 @@
 package hu.szamalk;
 
 public class Tabla {
+
+    private char[][] T;
+    private char uresCella;
+
+    public Tabla(char uresCella) {
+        T = new char[8][8];
+        uresCella = uresCella;
+
+        for (int i = 0; i < T.length; i++){
+            for (int j = 0; j < T[i].length; j++){
+                this.T[i][j] = this.uresCella;
+            }
+        }
+    }
+
+
 }
